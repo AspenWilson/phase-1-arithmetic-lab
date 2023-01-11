788e59f438d5725145f4a0a36f0e58322fc5a626
Aspen Wilson <aspen.schell@gmail.com>
2023-01-10 16:38:44 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1*num2;
 
-// Write your code here
+let random = (Math.ceil(Math.random()*10));
+
+const num3 = 31
+const num4 = 9
+const mod = num3 % num4; 
+
+let max = (Math.max(1, 3, 5, 8, 20));
+
+
+
